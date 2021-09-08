6ef62eeeec69c1ce9d90ab9a9939f5fedbf54ccb
Vernon Bengtson <tbengtson82@gmail.com>
2021-09-07 20:04:48 -0500

    Completed all

--- index.js
+++ index.js
@@ -271,8 +271,20 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(grade){
+  if (grade >= 90) {
+    return "you got an A";
+  }
+  else if (grade >= 80) {
+    return "you got a B";
+  }
+  else if (grade >= 70) {
+    return "you got a C";
+  }
+  else if (grade >= 60) {
+    return "you got a D";
+  }
+  else {return "you got an F";}
 }
 
 
